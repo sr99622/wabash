1679574495481af8456f36e1a0df6eaa7e6c636f
Stephen Rhodes <sr99622@gmail.com>
2025-10-31 23:41:22 -0400

    fix typo

--- scripts/windows/install_ffmpeg.bat
+++ scripts/windows/install_ffmpeg.bat
@@ -1,4 +1,4 @@
-curl -LO 'https://github.com/GyanD/codexffmpeg/releases/download/8.0/ffmpeg-8.0-full_build-shared.zip'
+curl -LO https://github.com/GyanD/codexffmpeg/releases/download/8.0/ffmpeg-8.0-full_build-shared.zip
 tar -xzf ffmpeg-8.0-full_build-shared.zip 
 move ffmpeg-8.0-full_build-shared ffmpeg
 copy ffmpeg\bin\*.dll wabash
